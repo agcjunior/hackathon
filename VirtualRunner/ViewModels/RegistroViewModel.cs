e4d81a0554192e506a6270112e5b876e0cf802f6
antonio jr <antonio.jr@ivia.com.br>
2017-11-17 14:49:30 -0300

    Implementação inicial

---
+++ VirtualRunner/ViewModels/RegistroViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualRunner.ViewModels
+{
+    public class RegistroViewModel
+    {
+        public string NomeCompleto { get; set; }
+        public string ComoQuerSerChamado { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string ConfirmacaoSenha { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Cep { get; set; }
+    }
+}
